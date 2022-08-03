26a39229783ede70209bb5e022138cf9d765bf48
ionelabrinza <ionela.brinza@yahoo.com>
2022-08-03 17:41:03 -0500

    declaring variables

---
+++ script.js
@@ -0,0 +1,38 @@
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+    }
+    clear() {
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined
+
+    }
+    delete() {
+
+    }
+    appendNumber() {
+
+    }
+    chooseOperation(operation) {
+
+    }
+    compute() {
+
+    }
+    updateDisplay() {
+
+    }
+}
+
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
